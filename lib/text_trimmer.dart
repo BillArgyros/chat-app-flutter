c4bea369d6fec5e02f57d5ae1bae70da95424e23
BillArgyros <bill.argyros55@gmail.com>
2022-03-01 23:25:05 +0200

    updated version

--- lib/text_trimmer.dart
+++
@@ -1,6 +0,0 @@
-//this method is responsible for clearing any with space before and after the beginning of the message
-String clearChatTextWhiteSpace(text) {
-  text = text.trimLeft();
-  text = text.trimRight();
-  return text;
-}
